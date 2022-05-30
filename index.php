f1dd3cb1c8ade964de4f2bd84fbdf44b50751e7e
Arpit <69616138+thepandeyaarpit@users.noreply.github.com>
2022-05-30 10:39:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+echo 'hello php';
+
+//  the main path is http://localhost/UrlRewriting/ or http://localhost/UrlRewriting/index.php but 
+// when we rewrite the URL the path is http://localhost/UrlRewriting/app.xml
+
+
+?>
---
+++ welcome.php
@@ -0,0 +1,12 @@
+<?php 
+
+    echo 'welcome page'
+
+?>
+
+
+<!-- the main path is http://localhost/UrlRewriting/ or http://localhost/UrlRewriting/index.php but 
+when we rewrite the URL the path is http://localhost/UrlRewriting/main.xml 
+
+we can edit this main.xml to something else like abcd.py, application etc...
+--> 
